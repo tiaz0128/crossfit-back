457c471fd5345521ffeb1a6821a2807af992a6a5
tiaz0128 <tiaz0128.dev@gmail.com>
2022-05-05 10:24:37 +0900

    DB config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from decouple import config
+
+DB_ENGINE = config('DB_ENGINE')
+DB_USERNAME = config('DB_USERNAME')
+DB_PASS = config('DB_PASS')
+DB_HOST = config('DB_HOST')
+DB_PORT = config('DB_PORT')
+DB_NAME = config('DB_NAME')
+
+SQLALCHEMY_DATABASE_URI = f'{DB_ENGINE}://{DB_USERNAME}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
